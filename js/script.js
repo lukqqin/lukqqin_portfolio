eb0a31c8f80907ebc04f95ac468af81c8e8f58fa
seikilla <polukina@stud.mediadesign.de>
2026-05-22 13:46:57 +0300

    pictures page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("Portfolio loaded");
+const galleryImages = document.querySelectorAll(".gallery-grid img");
+
+const fullscreen = document.querySelector(".fullscreen");
+
+const fullscreenImage = document.querySelector(".fullscreen img");
+
+galleryImages.forEach(image => {
+
+    image.addEventListener("click", () => {
+
+        fullscreen.classList.add("active");
+
+        fullscreenImage.src = image.src;
+
+    });
+
+});
+
+fullscreen.addEventListener("click", () => {
+
+    fullscreen.classList.remove("active");
+
+});
