5fdb391480652210422104837d87a20760324344
seikilla <polukina@stud.mediadesign.de>
2026-05-25 16:48:06 +0300

    update

--- js/script.js
+++ js/script.js
@@ -6,3 +6,36 @@ themeBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 
 });
+
+/* MODAL */
+
+const modal =
+document.getElementById("modal");
+
+const openModal =
+document.getElementById("openModal");
+
+const closeModal =
+document.getElementById("closeModal");
+
+openModal.addEventListener("click", () => {
+
+  modal.classList.add("active");
+
+});
+
+closeModal.addEventListener("click", () => {
+
+  modal.classList.remove("active");
+
+});
+
+modal.addEventListener("click", (e) => {
+
+  if (e.target === modal) {
+
+    modal.classList.remove("active");
+
+  }
+
+});
