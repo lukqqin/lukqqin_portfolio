b5f7d9d4f75658ba875e81fe5750c7778e57454c
seikilla <polukina@stud.mediadesign.de>
2026-05-25 17:23:09 +0300

    update

--- js/script.js
+++ js/script.js
@@ -37,5 +37,63 @@ modal.addEventListener("click", (e) => {
     modal.classList.remove("active");
 
   }
+});
+
+  /* CAROUSEL */
+
+const items =
+document.querySelectorAll(".carousel-item");
+
+const nextBtn =
+document.querySelector(".next");
+
+const prevBtn =
+document.querySelector(".prev");
+
+let current = 0;
+
+function updateCarousel() {
+
+  items.forEach((item, index) => {
+
+    item.classList.remove("active");
+
+    if (index === current) {
+
+      item.classList.add("active");
+
+    }
+
+  });
+
+}
+
+nextBtn.addEventListener("click", () => {
+
+  current++;
+
+  if (current >= items.length) {
+
+    current = 0;
+
+  }
+
+  updateCarousel();
 
 });
+
+prevBtn.addEventListener("click", () => {
+
+  current--;
+
+  if (current < 0) {
+
+    current = items.length - 1;
+
+  }
+
+  updateCarousel();
+
+});
+
+updateCarousel();
